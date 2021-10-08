25f9081617c232a8884125812647bad5dd482a3a
Kári Þrastarson <kari.thrastarson@islandsbanki.is>
2021-10-08 22:56:11 +0000

    Revert "getArchitectAssets"
    This reverts commit 7b1f6389c28ef124ab0d16617dbb1c4ed8c7f317.

--- src/main/java/com/karithrastarson/registry/service/ArchitectService.java
+++ src/main/java/com/karithrastarson/registry/service/ArchitectService.java
@@ -1,24 +1,17 @@
 package com.karithrastarson.registry.service;
 
 import com.karithrastarson.registry.entity.Architect;
-import com.karithrastarson.registry.entity.Asset;
 import com.karithrastarson.registry.exception.DuplicateException;
 import com.karithrastarson.registry.repository.ArchitectRepository;
-import com.karithrastarson.registry.repository.AssetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ArchitectService {
     @Autowired
     ArchitectRepository architectRepository;
-
-    @Autowired
-    AssetRepository assetRepository;
 
     public Architect addArchitect(String name, String uni, String dob) throws DuplicateException {
         //Check if this entry already exists (same name and dob)
@@ -47,22 +40,8 @@ public class ArchitectService {
      * @return A list of links to assets
      */
     public List<String> getArchitectAssets(Long id) {
-        Optional<Architect> results = architectRepository.findById(id);
-        if(!results.isPresent()) {
-            return null;
-        }
-        List<Long> assetIds = results.get().getAssets();
-        if (assetIds == null) {
-            return null;
-        }
-        List<String> urls = new ArrayList<>();
-        for(long assetId : assetIds) {
-            Optional<Asset> asset = assetRepository.findById(assetId);
-            if (asset.isPresent()) {
-                urls.add(asset.get().getUrl());
-            }
-        }
-        return urls;
+       //TODO:IMPLEMENT
+        return null;
     }
 
     public void addAsset(Long architectId, long assetId) {
